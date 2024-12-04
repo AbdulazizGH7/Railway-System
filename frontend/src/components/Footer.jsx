3951a7f2b2e8870fd9266e43d2e0eef68a7bb124
Abdulaziz <abdulazizgh.75@gmail.com>
2024-12-04 12:29:53 +0300

    Created the Footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-cyan-600 p-6 text-center mt-auto'>
+    <a
+    href="mailto:s202162490@kfupm.edu.sa"
+    className='text-gray-100 font-light text-lg'>
+    Contact Us</a>
+</footer>
+  )
+}
+
+export default Footer

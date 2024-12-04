3f487e32494b3ae8ef5fbb5f7a74d40c9dd81f4b
MEID <Theycallmemohammed@gmail.com>
2024-12-04 22:07:18 +0300

    reservation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from './layouts/MainLayout';
 import BookingPage from './pages/BookingPage'
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignupPage";
+import ReservationPage from "./pages/ReservationPage";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -13,7 +14,7 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route path="/" element={<h1>Home</h1>}/>
           <Route path="/booking" element={<BookingPage/>}/>
-          
+          <Route path="/reserve" element={<ReservationPage />} />
         </Route>
 
       </>

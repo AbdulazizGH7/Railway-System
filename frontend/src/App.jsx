c3cf782f11299996ffdfb9f4de49453be1bd0bb8
MEID <Theycallmemohammed@gmail.com>
2024-12-04 20:44:34 +0300

    login/signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import MainLayout from './layouts/MainLayout';
 import BookingPage from './pages/BookingPage'
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignupPage";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
+      
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
         <Route path="/" element={<MainLayout />}>
           <Route path="/" element={<h1>Home</h1>}/>
           <Route path="/booking" element={<BookingPage/>}/>
+          
         </Route>
+
       </>
     )
   )

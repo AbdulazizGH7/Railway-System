3daf51452eaa4181bac196147f477eb6973fab56
Abdulaziz <abdulazizgh.75@gmail.com>
2024-12-04 12:33:19 +0300

    Added few temporary routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
-
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import MainLayout from './layouts/MainLayout';
+import BookingPage from './pages/BookingPage'
 function App() {
-  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<h1>Home</h1>}/>
+          <Route path="/booking" element={<BookingPage/>}/>
+        </Route>
+      </>
+    )
+  )
+  return (
+    <>
+    <RouterProvider router={router}/>
+    </>
+  )
 }
 
 export default App

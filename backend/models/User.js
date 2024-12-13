e678fa78672e4aa00877f0c8e01a746977099d08
Abdulaziz <abdulazizgh.75@gmail.com>
2024-12-13 14:09:14 +0300

    Added loyalty tiers and nationalId

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,40 @@
-const mongoose = require('mongoose');  
+const mongoose = require('mongoose');
+
+const LOYALTY_TIERS = {
+  REGULAR: 'Regular',
+  GREEN: 'Green',
+  SILVER: 'Silver',
+  GOLD: 'Gold'
+};
+
+const TIER_THRESHOLDS = {
+  GREEN: 10000,   // 10K miles
+  SILVER: 50000,  // 50K miles
+  GOLD: 100000    // 100K miles
+};
 
 // Base User Schema  
-const userSchema = new mongoose.Schema({  
+const userSchema = new mongoose.Schema({ 
+  nationalId:{
+    type: Number,
+    unique: true,
+    required: true
+  }, 
   email: {  
     type: String,  
-    required: true,  
     unique: true  
   },  
   password: {  
-    type: String,  
-    required: true  
+    type: String  
   },  
-  firstName: String,  
-  lastName: String,  
+  firstName:{
+    type: String,
+    required: true
+  },  
+  lastName: {
+    type: String,
+    required: true
+  },  
   role: {  
     type: String,
     enum: ['passenger', 'admin', 'driver', 'engineer'],  
@@ -22,9 +44,36 @@ const userSchema = new mongoose.Schema({
   loyaltyPoints: {  
     type: Number,  
     default: 0  
-  },    
+  },
+  loyaltyTier: {
+    type: String,
+    enum: ['Regular', 'Green', 'Silver','Gold'],
+    default: "Regular"
+  }    
 })
+
+userSchema.pre('save', function(next) {
+  // Update loyalty tier based on points
+  if (this.loyaltyPoints >= TIER_THRESHOLDS.GOLD) {
+    this.loyaltyTier = 'Gold';
+  } else if (this.loyaltyPoints >= TIER_THRESHOLDS.SILVER) {
+    this.loyaltyTier = 'Silver';
+  } else if (this.loyaltyPoints >= TIER_THRESHOLDS.GREEN) {
+    this.loyaltyTier = 'Green';
+  } else {
+    this.loyaltyTier = "Regular";
+  }
+  next();
+});
+
+userSchema.methods.addLoyaltyPoints = function(points) {
+  this.loyaltyPoints += points;
+  return this.save(); // This will trigger the pre-save middleware
+};
   
 const User = mongoose.model('User', userSchema);  
 
-module.exports = User
+module.exports = {
+  User,
+  LOYALTY_TIERS,
+};
